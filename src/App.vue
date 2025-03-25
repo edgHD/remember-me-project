43f88f9c4c07e2264c92196e2ab4429ca99dc231
Edgar <edgaarr2005@hotmail.com>
2025-03-25 13:43:13 -0400

    I created ListItem.vue component and connected it with props so it displays the storedResources content.

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,29 @@
 <template>
-  <ul></ul>
+  <ListItem :items="storedResources" />
 </template>
 
 <script>
+import ListItem from './components/ListItem.vue'
 export default {
+  components: {
+    ListItem,
+  },
   data() {
     return {
-      storedResources: [{
-        id: this.$uuid(),
-        title: 'Official Guide',
-        description: 'The official Vue.js documentation.',
-        link: 'https://vuejs.org'
-      },
-      {
-        id: this.$uuid(),
-        title: 'Crunchyroll',
-        description: 'Watch a complete library of anime.',
-        link: 'https://crunchyroll.com'
-      }],
+      storedResources: [
+        {
+          id: this.$uuid(),
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation.',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: this.$uuid(),
+          title: 'Crunchyroll',
+          description: 'Watch a complete library of anime.',
+          link: 'https://crunchyroll.com',
+        },
+      ],
     }
   },
 }
---
+++ src/components/ListItem.vue
@@ -0,0 +1,58 @@
+<template>
+    <ul>
+        <li v-for="item in items" :key="item.id">
+            <h3>{{ item.title }}</h3>
+            <p>{{ item.description }}</p>
+            <a :href="item.link">View Resource</a>
+        </li>
+    </ul>
+</template>
+
+<script>
+export default {
+    props: ['items'],
+}
+</script>
+
+<style scoped>
+* {
+    font-family: Arial, Helvetica, sans-serif;
+}
+
+ul {
+    list-style-type: none;
+    padding: 0;
+    margin: 0;
+}
+
+li {
+    margin: 16px 0;
+    padding: 16px;
+    background-color: #f9f9f9;
+    border: 1px solid #ddd;
+    border-radius: 8px;
+}
+
+h3 {
+    margin: 0 0 8px;
+    font-size: 1.5rem;
+    color: #333;
+}
+
+p {
+    margin: 0 0 12px;
+    font-size: 1rem;
+    color: #666;
+}
+
+a {
+    font-size: 1rem;
+    color: #007BFF;
+    text-decoration: none;
+    transition: color 0.2s ease-in-out;
+}
+
+a:hover {
+    color: #00b953;
+}
+</style>
